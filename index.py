7a68d0ebfd956904d7f276217598e906d57371cc
Andres Acosta Garcia <andresfelipeacostagarcia34@gmail.com>
2024-11-28 22:30:39 -0500

    getting started

---
+++ index.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("front.html", title="titulo", description="descripcion")
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=8000)
+
