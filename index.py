c3884a3c03ca1708d17b8738c65de262ca5a3178
Andres Acosta Garcia <andresfelipeacostagarcia34@gmail.com>
2024-11-29 19:20:47 -0500

    added images in card formats

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
+from faker import Faker
 
-
+fake=Faker()
+list=("name","address","date","country","text","location","credit card","email","phone number","color","company")
 app = Flask(__name__)
+length=len(list)
 
 @app.route("/")
-def hello_world():
-    return render_template("front.html", title="titulo", description="descripcion")
+def index():
+    return render_template("front.html", lista=list, length=length)
 
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8000)
-
