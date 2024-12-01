43182fe6b60f2919f78ccdf0ab735c35c4e17143
Andres Acosta Garcia <andresfelipeacostagarcia34@gmail.com>
2024-12-01 17:45:22 -0500

    adding inherited layout

--- index.py
+++ index.py
@@ -2,13 +2,20 @@ from flask import Flask, render_template
 from faker import Faker
 
 fake=Faker()
-list=("name","address","date","country","text","location","credit card","email","phone number","color","company")
+list=("name","address","calendar","countries","text","credit_card","email","phone_number","colors","company")
 app = Flask(__name__)
 length=len(list)
 
 @app.route("/")
 def index():
     return render_template("front.html", lista=list, length=length)
+
+@app.route("/allow/<selection>")
+def selection(selection):
+    if not list.__contains__(selection):
+        return "Wrong selection"
+    else:
+        return render_template(selection+".html", selection=selection)        
 
 
 if __name__ == '__main__':
