7cf92f12d6bfe37d1f7aabad4009d371ed5ae276
Andres Acosta Garcia <andresfelipeacostagarcia34@gmail.com>
2025-01-10 19:59:07 -0500

    adding templates

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 from faker import Faker
 import datetime
+import csv
+from helpers import name
+import json
+import urllib.parse
+
 
 fake=Faker()
 list=("name","address","date","text","credit_card","email","phone_number","colors","company")
@@ -20,24 +25,27 @@ def selection(selection):
 
 @app.route("/allow/<results>/<int:digit>")
 def results(results, digit):
-    """
-    instances=[getattr(fake,results)() for i in range(digit)]
     instancess=[]
-        
-    
+    instances=[getattr(fake,results)() for i in range(digit)]
     for i in instances:
-        if(isinstance(i, datetime.date)):
-            instancess.append(i.strftime("%Y-%m-%d"))  
-        else:
-            instancess=instances
-              
-    
-    
+        instancess.append([i])
+
     return render_template("results.html", results=instancess, digit=digit) 
-    """
-    return results
 
-
+@app.route("/download/<params>/")
+def download(params):
+    decoded_params = urllib.parse.unquote(params)
+    try:
+        allData = json.loads(decoded_params)
+        # Specify the file name
+        filename = "output.csv"
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            for row in allData:
+                writer.writerow([row])
+    except json.JSONDecodeError:
+        return "Invalid JSON format"    
+    return "Downloaded"
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8000)
