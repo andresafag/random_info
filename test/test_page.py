60c8e4af43511780bb3a4baea4f0c44429ee4dd7
Andres Acosta Garcia <andresfelipeacostagarcia34@gmail.com>
2025-03-06 21:54:25 -0500

    ho

--- test/test_page.py
+++ test/test_page.py
@@ -16,6 +16,7 @@ def test_add():
     assert sum.add(2, 3) == 5
     assert sum.add(-1, 1) == 0
     assert sum.add(0, 0) == 0
+    assert sum.add(1, 3) == 10
 
 if __name__ == "__main__":
     pytest.main()
