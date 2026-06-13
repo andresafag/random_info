c74ac4a7e07e0d081e7ffd899390caee4eaa2f5e
andres acosta <andresfelipeacostagarcia34@gmail.com>
2026-06-13 10:06:40 -0400

    add: docker build and ousg steps in pipeline

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,11 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "randomize-it-688567305851-us-east-1-an"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-
-
-    # Habilita el bloqueo de estado nativo de S3 (sin necesidad de DynamoDB)
+    key          = "terraform.tfstate"
     use_lockfile = true
   }
   required_version = ">= 1.0"
