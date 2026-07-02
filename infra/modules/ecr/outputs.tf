53731db4687b83712a797f7266af34a5ebe184f1
andres acosta <andresfelipeacostagarcia34@gmail.com>
2026-07-02 10:36:01 -0400

    fix: lambda image push

--- infra/modules/ecr/outputs.tf
+++ infra/modules/ecr/outputs.tf
@@ -2,6 +2,6 @@ output "repository_url" {
   value = aws_ecr_repository.this.repository_url
 }
 
-output "image_push_id" {
-  value = null_resource.docker_build_and_push.id
-}
+# output "image_push_id" {
+#   value = null_resource.docker_build_and_push.id
+# }
